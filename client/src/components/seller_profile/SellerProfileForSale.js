6bf51434844f7208e5dd92cce595a315315bcab2
ryan <ryan.d.matuszak@gmail.com>
2021-03-29 16:36:13 -0500

    made the seller info page adapt to a larger screen size using MUI media query. Started adding bid/buy buttons functionality. began refactoring seller info main component so that only table data is needed and not user data and table data

--- client/src/components/seller_profile/SellerProfileForSale.js
+++ client/src/components/seller_profile/SellerProfileForSale.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
-import { Grid } from "@material-ui/core";
+import { 
+  Grid, 
+  Button, 
+  Modal, 
+  FormControl, 
+  InputLabel, 
+  Input,
+  Dialog,
+  DialogTitle,
+  DialogActions
+
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import './sellerProfile.css';
 import Table from '@material-ui/core/Table';
@@ -10,6 +21,8 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useDispatch, useSelector, connect } from "react-redux";
 
 const useStyles = makeStyles((theme) => ({
   grid: {
@@ -40,6 +53,30 @@ const useStyles = makeStyles((theme) => ({
     padding: '10px',
     height: '300px',
     width: '300px',
+    marginBottom: '200px',
+    paddingRight: '50px'
+  },
+  paper_large_screen: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    backgroundColor: 'white',
+    background: 'white',
+    color: theme.palette.secondary.contrastText,
+    height: '200px',
+    width: '200px',
+    marginLeft: '20px',
+    paddingTop: '50px',
+    paddingBottom: '50px',
+    paddingLeft: '20px'
+  },
+  image_large_screen: {
+    display: "flex",
+    justifyContent: "center",
+    alignSelf: 'center',
+    alignItems: "center",
+    padding: '10px',
+    height: '200px',
+    width: '200px',
     marginBottom: '200px',
     paddingRight: '50px'
   },
@@ -105,30 +142,120 @@ const useStyles = makeStyles((theme) => ({
 // rgb(206, 204, 204)
 
 const SellerProfileForSale = (props) => {
-  let itemData = props.itemData['user_data']['items_for_sale']
-  let tableData = props.itemData['table_data']
-  let user = props.itemData['user_data']['user']['username']
-  //('USER:', user)
-  const classes = useStyles()
+  const currUserId = useSelector(store => store.session.currentUser.id);
+  // let items = useSelector(store => store.entities.items_state.saleItems);
+  let itemData = props.itemData['user_data']['items_for_sale'];
+  let tableData = props.itemData['table_data'];
+  let user = props.itemData['user_data']['user']['username'];
+  const [bidInfo, setBidInfo] = useState({})
+  const [bidInput, setBidInput] = useState(null);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [currItemId, setCurrItemId] = useState(null);
+  const [currBid, setCurrBid] = useState(null);
+  const [currItemPrice, setCurrItemPrice] = useState(null);
+  const [propsItemDataArrayIdx, setPropsItemDataArrayIdx] = useState(null)
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const largeScreen = useMediaQuery('(min-width:1870px)');
+
+  console.log('TABLE DATA:', tableData)
+
+  const handleDialogOpen = (itemData) => {
+    setCurrItemId(itemData.itemId)
+    setDialogOpen(true)
+  };
+
+  const handleDialogClose = () => {
+    setDialogOpen(false);
+  };
+
+  const openBidModal = (itemData) => {
+    setCurrItemId(itemData.itemId)
+    setCurrBid(itemData.currentBid)
+    setCurrItemPrice(itemData.itemPrice)
+    setPropsItemDataArrayIdx(itemData.idx)
+    setModalOpen(true)
+  };
+
+  const closeModal = () => {
+    setModalOpen(false)
+  };
+
+  const updateBidInput = (e) => {
+    setBidInput(e.target.value)
+  };
+
+  const updateItems = (updatedItem) => {
+    props.itemData['table_data'][propsItemDataArrayIdx].current_bid = updatedItem.current_bid
+  };
+
+  const submitBid = async () => {
+    const body = {
+      bidInput,
+      currUserId
+    }
+    const res = await fetch(`http://localhost:5000/api/items-and-services/${currItemId}/bid`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    })
+    const updatedItem = await res.json()
+    updateItems(updatedItem)
+    setModalOpen(false)
+  };
+
+  const updateSoldItems = (id) => {
+    // let currItems = []
+    // items.forEach((item, i) => {
+    //   if(Number(item.id) !== Number(id)) {
+    //     currItems.push(item)
+    //   }
+    //   dispatch(setItems(currItems))
+    // })
+  }
+
+  const handlePurchase = async () => {
+    const body = {
+      currUserId
+    }
+    const res = await fetch(`http://localhost:5000/api/items-and-services/${currItemId}/purchase`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    })
+    const {soldItemId} = await res.json()
+    updateSoldItems(soldItemId)
+    handleDialogClose()
+  };
+
   return(
     <>
       <div className="divider">
       </div>
-      {itemData.length > 0 ?
-        <div className="seller-items-body-container">
+      {tableData.length > 0 ?
             <Grid container spacing={3} className={classes.grid} >
-              {itemData.map((item, idx) => {
+              {tableData.map((item, idx) => {
+                console.log(item)
+                bidInfo[item.id] = item.current_bid
                 let url = item.image_url
                 return (
                   <>
-                    <Grid item xs={12} md={12} lg={12} className={classes.gridItem} key={idx}>
-                      <div className="item-name-seller-profile"><h2 className="item-text">{item.name}</h2></div>
+                  <Grid item xs={12} md={12} lg={largeScreen ? 6 : 12} className={classes.gridItem} key={idx}>
+                    <div className="seller-items-body-container">
                         <div className="seller-page-item-cards">
-                          <Card className={classes.paper}>
-                            <CardContent className={classes.image}>
-                              <img className="item-image" src={url} />
-                            </CardContent>
-                          </Card>
+                          <div className="image-container-seller-profile">
+                          <div className="item-name-seller-profile"><h2 className="item-text">{item.name}</h2></div>
+                            <Card className={largeScreen ? classes.paper_large_screen : classes.paper}>
+                              <CardContent className={largeScreen ? classes.image_large_screen : classes.image}>
+                                <img className="item-image" src={url} />
+                              </CardContent>
+                            </Card>
+                          </div>
                           <div className="description-table-container">
                             <div className="table-container">
                               <TableContainer className={classes.tableContainer}>
@@ -144,10 +271,10 @@ const SellerProfileForSale = (props) => {
                                   </TableHead>
                                   <TableBody>
                                     <TableRow key={tableData[idx].name}>
-                                      <TableCell align="center">${tableData[idx].price}</TableCell>
-                                      <TableCell align="center">${tableData[idx].bid}</TableCell>
-                                      <TableCell align="center">{tableData[idx].num_bidders}</TableCell>
-                                      <TableCell align="center">{tableData[idx].days_remaining}</TableCell>
+                                      <TableCell align="center">${item.price}</TableCell> 
+                                      <TableCell align="center">${item.current_bid}</TableCell>
+                                      <TableCell align="center">{item.num_bidders}</TableCell>
+                                      <TableCell align="center">{item.days_remaining}</TableCell>
                                     </TableRow>
                                   </TableBody>
                                 </Table>
@@ -158,24 +285,96 @@ const SellerProfileForSale = (props) => {
                             </div>
                           </div>
                           <div className="bid-buy-buttons-container-seller-profile">
-
+                            <div className="bid-button">
+                              <Button color="secondary" variant="contained" onClick={() => {openBidModal({'itemId': item.item_id, 'currentBid': item.current_bid, 'itemPrice': item.price, 'idx': idx})}} className={classes.buttons}>
+                                Bid
+                              </Button>
+                            </div>
+                            <div className="divider-container">
+                              <div className="bid-purchase-divider"></div>
+                            </div>
+                            <div className="buy-button">
+                              <Button color="secondary" size="medium" variant="contained" onClick={() => {handleDialogOpen({'itemId': item.id, 'currentBid': item.current_bid, 'itemPrice':item.price})}} className={classes.buttons}>
+                                Purchase
+                              </Button>
+                            </div>
+                            <div className="divider-container">
+                              <div className="bid-purchase-divider"></div>
+                            </div>
                           </div>
                         </div>
+                        </div>
                     </Grid>
-                    <div className="divider">
-                    </div>
+                    {/* <div className="divider">
+                    </div> */}
                   </>
                 )
               })}
             </Grid>
-        </div>
        :
        <div className="no-items-message-seller-profile">
          <h1>No Items Have Been Posted For Sale by This User</h1>
        </div>
       }
+
+      <Modal
+        open={modalOpen}
+        onClose={closeModal}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        <div className={classes.itemFormModal}>
+          <h2 id="simple-modal-title">Place Your Bid:</h2>
+          <div>
+            <FormControl>
+              <InputLabel htmlFor="bid-input" style={{color: "black"}}>Bid Amount:</InputLabel>
+              <Input id="bid-input" onChange={updateBidInput} autoFocus style={{color: "black"}}/>
+            </FormControl>
+          </div>
+          <div>
+            <Button
+              variant="contained"
+              color="secondary"
+              style={{ color: "white" }}
+              size="small"
+              className={classes.submitButton}
+              onClick={() => {
+                if(Number(bidInput) <= currBid) {
+                  alert('Your bid must be larger than the current bid amount')
+                } else if(Number(bidInput) > currItemPrice) {
+                  alert('Your bid must be less than the item sell price')
+                } else {
+                  submitBid()
+                }
+              }}
+              type="submit"
+            >
+              Submit Bid
+            </Button>
+          </div>
+        </div>
+      </Modal>
+
+      <Dialog
+          open={dialogOpen}
+          onClose={handleDialogClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">
+              {"Are you sure that you want to purchase this item at its full sale price?"}
+            </DialogTitle>
+            <DialogActions>
+              <Button onClick={handleDialogClose} className={classes.buttons} color="secondary" variant="contained">
+                Cancel
+              </Button>
+              <Button className={classes.buttons} color="secondary" variant="contained" autoFocus onClick={handlePurchase}>
+                Purchase Item
+              </Button>
+            </DialogActions>
+          </Dialog>
     </>
   )
-}
+};
 
 export default SellerProfileForSale;
